d2c7417ff27915320f220037f419f71384fcde7d
Dan Foley <dan@dan-foley.com>
2020-01-28 18:21:08 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,10 @@ def remove_strawberry(contacts)
     # binding.pry
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
-        binding.pry
+        if attribute == :favorite_ice_cream_flavors
+        
+          binding.pry
+        end
       end
     end
   end
