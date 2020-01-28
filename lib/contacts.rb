c5af0d81fe641bb346b5308a2dbc977cafa712fc
Dan Foley <dan@dan-foley.com>
2020-01-28 18:20:00 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,6 +17,8 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     # binding.pry
-    
+    if person == "Freddy Mercury"
+      
+    end
   end
 end
